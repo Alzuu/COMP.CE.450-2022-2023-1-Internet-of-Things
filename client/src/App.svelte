df439f455831c2628183d8040be324d3854ab2df
Allan Li <allan.li@tuni.fi>
2023-03-24 12:11:03 +0200

    Restructure project and additional setup

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,47 +1,48 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from 'svelte';
+  import { createBluetooth } from 'node-ble';
+
+  const { bluetooth, destroy } = createBluetooth();
+
+  let data = [];
+
+  onMount(async () => {
+    await setupBLE();
+  });
+
+  async function setupBLE() {
+    // Get adapter
+    const adapter = await bluetooth.defaultAdapter();
+
+    // Start discovering
+    if (!(await adapter.isDiscovering())) {
+      await adapter.startDiscovery();
+    }
+
+    // Get a device, Connect and Get GATT server
+    const device = await adapter.waitDevice('uuid');
+    await device.connect();
+    const gattServer = await device.gatt();
+
+    // Subscribe to a characteristic
+    const service = await gattServer.getPrimaryService('uuid');
+    const characteristic = await service.getCharacteristic('uuid');
+    await characteristic.startNotifications();
+    characteristic.on('valuechanged', (buffer) => {
+      console.log(buffer);
+      data.push(buffer);
+    });
+  }
+
+  async function stop(device) {
+    await device.disconnect();
+    destroy();
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <h1>Proximity analyzer</h1>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
